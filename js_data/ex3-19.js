427010057809fcec63fabaff0bc3f5320d37827f
= <=>
2023-09-08 18:42:38 +0900

    2023.09.08 수업

---
+++ js_data/ex3-19.js
@@ -0,0 +1,12 @@
+for(let i=2;i<11;i++){
+    console.log("=================" );
+    for(let j=1;j<=9;j++){
+      
+        // i+ '*'+j +"=" +i*j;
+
+    
+    console.log( i+ '*'+j +"=" +i*j);
+        
+    }
+}
+
---
+++ js_data/p340.js
@@ -0,0 +1,6 @@
+let age = 10;
+let admissionFee = '';
+if(age<13){
+    admissionFee = '2,000원';
+}
+console.log(admissionFee);
